ca627e9a8f26b3b93d5a86209cada79b30eec794
Miłosz Kordziński <152620516+milekv@users.noreply.github.com>
2024-11-06 08:41:14 +0100

    schema.sql

---
+++ src/db/schema.sql
@@ -0,0 +1,13 @@
+/firma-szkoleniowa
+├── README.md
+├── src
+│   ├── db
+│   │   ├── schema.sql
+│   │   ├── queries.sql
+│   │   ├── procedures.sql
+│   │   └── triggers.sql
+│   └── web
+│       ├── app.py
+│       ├── templates
+│       │   ├── index.html
+│       │   └── rejestracja.html
